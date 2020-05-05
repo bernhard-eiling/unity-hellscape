0f36ac130faae0e434bfd4f42af6d7476230cd29
Bernhard Eiling <bernhard.eiling@gmail.com>
2020-05-05 23:34:29 +0200

    Fixed ridge

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -10,12 +10,16 @@ public class TerrainGenerator : MonoBehaviour
         data = GetComponent<Terrain>().terrainData;
         int width = data.heightmapResolution;
         int height = data.heightmapResolution;
-        float[,] map1 = HeightMap(new float[width, height], 5F, 0.4F);
-        float[,] ridged = Ridged(map1, 0.15F);
-        // float[,] map2 = HeightMap(map1, 15F, 0.05F);
-        // float[,] map3 = HeightMap(map2, 50F, 0.007F);
-        // float[,] map4 = HeightMap(map3, 100F, 0.004F);
-        data.SetHeights(0, 0, ridged);
+        float[,] map1 = HeightMap(new float[width, height], 3F, 0.4F);
+
+        float[,] map2 = HeightMap(map1, 15F, 0.05F);
+        float[,] ridged1 = Ridged(map2, 0.20F);
+
+        float[,] map3 = HeightMap(ridged1, 50F, 0.007F);
+        float[,] map4 = HeightMap(map3, 100F, 0.003F);
+        float[,] map5 = HeightMap(map4, 300F, 0.004F);
+
+        data.SetHeights(0, 0, map5);
     }
 
     void Update()
@@ -27,9 +31,11 @@ public class TerrainGenerator : MonoBehaviour
         for (int h = 0; h < height; h++) {
             for (int w = 0; w < width; w++) {
                 float baseValue = baseMap[w, h];
-                Debug.Log(baseValue);
                 if (baseValue > inversionThreshold) {
-                    baseMap[w, h] = 1.0F - baseValue;
+                    baseValue -= inversionThreshold;
+                    baseValue *= -1.0F;
+                    baseValue += inversionThreshold;
+                    baseMap[w, h] = baseValue;
                 }
             }
         }
