9b68f182f9510f25dc3bef5f4bca32d4a62a8a60
bernhard-the-wiser <bernhard.eiling@transferwise.com>
2023-06-07 22:35:16 +0100

    Return void for height map setting methods

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -8,24 +8,25 @@ public class TerrainGenerator : MonoBehaviour
     void Start()
     {
         data = GetComponent<Terrain>().terrainData;
+
         int width = data.heightmapResolution;
         int height = data.heightmapResolution;
-        float[,] map1 = HeightMap(new float[width, height], 3F, 0.4F);
+        float[,] heights = new float[width, height];
 
-        float[,] map2 = HeightMap(map1, 15F, 0.05F);
-        float[,] ridged1 = Ridged(map2, 0.20F);
+        HeightMap(heights, 3F, 0.4F);
+        HeightMap(heights, 15F, 0.05F);
+        Ridged(heights, 0.20F);
 
-        float[,] map3 = HeightMap(ridged1, 50F, 0.007F);
-        float[,] map4 = HeightMap(map3, 100F, 0.003F);
-        float[,] map5 = HeightMap(map4, 300F, 0.004F);
+        HeightMap(heights, 50F, 0.007F);
+        HeightMap(heights, 100F, 0.003F);
+        HeightMap(heights, 300F, 0.004F);
 
-        data.SetHeights(0, 0, map5);
+        data.SetHeights(0, 0, heights);
     }
 
-    void Update()
-    {}
+    void Update() {}
 
-    float[,] Ridged(float[,] baseMap, float inversionThreshold) {
+    void Ridged(float[,] baseMap, float inversionThreshold) {
         int width = baseMap.GetLength(0);
         int height = baseMap.GetLength(1);
         for (int h = 0; h < height; h++) {
@@ -39,10 +40,9 @@ public class TerrainGenerator : MonoBehaviour
                 }
             }
         }
-        return baseMap;
     }
- 
-    float[,] HeightMap(float[,] baseMap, float planeScale, float heightScale) {
+
+    void HeightMap(float[,] baseMap, float planeScale, float heightScale) {
         int width = baseMap.GetLength(0);
         int height = baseMap.GetLength(1);
         for (int h = 0; h < height; h++) {
@@ -54,6 +54,5 @@ public class TerrainGenerator : MonoBehaviour
                 baseMap[w, h] = baseValue + randValue;
             }
         }
-        return baseMap;
     }
 }
